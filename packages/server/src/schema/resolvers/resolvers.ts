b552a7f32934f22a76a16457ca480b5c656c6196
Brendan Healey <healey_brendan@yahoo.co.uk>
2023-01-14 04:44:13 +0000

    Remove some ununsed code

--- packages/server/src/schema/resolvers/resolvers.ts
+++ packages/server/src/schema/resolvers/resolvers.ts
@@ -1,16 +1,7 @@
-import * as fs from "fs";
-import getQuery from "schema/resolvers/getQuery";
 import { getUserResolver } from "schema/resolvers/getUserResolver";
 import { secured } from "schema/resolvers/secured";
 import { userLoginResolver } from "schema/resolvers/userLoginResolver";
 import { ServerContext } from "server";
-
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-
-const privateKey = fs.readFileSync("./keys/private.key", "utf8");
-
-const getUserSql = "SELECT * FROM `users` WHERE email = ?";
 
 export const resolvers = {
   Query: {
